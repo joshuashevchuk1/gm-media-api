6e844541550d34ed5316855e0d743b702e181011
Tyler Albert <tyleralbert@google.com>
2025-01-15 15:35:12 -0800

    feat: Add ConferenceDataChannel class for stateful C++ implementation of Meet Media API client
    PiperOrigin-RevId: 715973962

---
+++ native_with_state/internal/conference_data_channel.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NATIVE_WITH_STATE_INTERNAL_CONFERENCE_DATA_CHANNEL_H_
+#define NATIVE_WITH_STATE_INTERNAL_CONFERENCE_DATA_CHANNEL_H_
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "native_with_state/api/media_api_client_interface.h"
+#include "native_with_state/internal/conference_data_channel_interface.h"
+#include "native_with_state/internal/resource_handler_interface.h"
+#include "webrtc/api/data_channel_interface.h"
+#include "webrtc/api/scoped_refptr.h"
+
+namespace meet {
+
+// A wrapper around a `webrtc::DataChannelInterface` that provides a simplified
+// interface for sending resource requests and receiving resource updates.
+//
+// This class closes the underlying data channel when it is destroyed.
+class ConferenceDataChannel : public ConferenceDataChannelInterface,
+                              public webrtc::DataChannelObserver {
+ public:
+  ConferenceDataChannel(
+      std::unique_ptr<ResourceHandlerInterface> resource_handler,
+      rtc::scoped_refptr<webrtc::DataChannelInterface> data_channel)
+      : resource_handler_(std::move(resource_handler)),
+        data_channel_(std::move(data_channel)) {
+    data_channel_->RegisterObserver(this);
+  };
+
+  ~ConferenceDataChannel() override { data_channel_->Close(); }
+
+  // ConferenceDataChannel is neither copyable nor movable.
+  ConferenceDataChannel(const ConferenceDataChannel&) = delete;
+  ConferenceDataChannel& operator=(const ConferenceDataChannel&) = delete;
+
+  void OnStateChange() override {
+    LOG(INFO) << "ConferenceDataChannel::OnStateChange: "
+              << data_channel_->state();
+  };
+
+  void OnMessage(const webrtc::DataBuffer& buffer) override;
+
+  // Future WebRTC updates will force this to always be true. Ensure that
+  // current behavior reflects desired future behavior.
+  bool IsOkToCallOnTheNetworkThread() override { return true; }
+
+  // Sets the callback for receiving resource updates from the resource data
+  // channel.
+  //
+  // The callback is called on the associated peer connection's network thread.
+  //
+  // Resource data channels can only have one callback at a time, and the
+  // callback must outlive the resource data channel if one is set.
+  //
+  // Setting the callback is not thread-safe, so it should only be called before
+  // the resource data channel is used (i.e. before the peer connection is
+  // started).
+  void SetCallback(ResourceUpdateCallback callback) override {
+    callback_ = std::move(callback);
+  }
+
+  absl::Status SendRequest(ResourceRequest request) override;
+
+ private:
+  std::string label() const { return data_channel_->label(); }
+
+  ResourceUpdateCallback callback_;
+  std::unique_ptr<ResourceHandlerInterface> resource_handler_;
+  rtc::scoped_refptr<webrtc::DataChannelInterface> data_channel_;
+};
+
+}  // namespace meet
+
+#endif  // NATIVE_WITH_STATE_INTERNAL_CONFERENCE_DATA_CHANNEL_H_
---
+++ native_with_state/internal/conference_data_channel_test.cc
@@ -0,0 +1,245 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "native_with_state/internal/conference_data_channel.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <variant>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "testing/base/public/mock-log.h"
+#include "absl/base/log_severity.h"
+#include "absl/functional/any_invocable.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "native_with_state/api/media_api_client_interface.h"
+#include "native_with_state/api/session_control_resource.h"
+#include "native_with_state/internal/resource_handler_interface.h"
+#include "webrtc/api/data_channel_interface.h"
+#include "webrtc/api/rtc_error.h"
+#include "webrtc/api/scoped_refptr.h"
+#include "webrtc/api/test/mock_data_channel.h"
+
+namespace meet {
+namespace {
+
+using ::base_logging::ERROR;
+using ::base_logging::WARNING;
+using ::testing::_;
+using ::testing::HasSubstr;
+using ::testing::kDoNotCaptureLogsYet;
+using ::testing::MockFunction;
+using ::testing::NotNull;
+using ::testing::Return;
+using ::testing::ScopedMockLog;
+using ::testing::StrEq;
+using ::testing::status::StatusIs;
+
+class MockResourceHandler : public ResourceHandlerInterface {
+ public:
+  MOCK_METHOD(absl::StatusOr<ResourceUpdate>, ParseUpdate,
+              (absl::string_view update), (override));
+  MOCK_METHOD(absl::StatusOr<std::string>, StringifyRequest,
+              (const ResourceRequest &request), (override));
+};
+
+TEST(ConferenceDataChannelTest, SendRequestSucceeds) {
+  auto resource_handler = std::make_unique<MockResourceHandler>();
+  EXPECT_CALL(*resource_handler, StringifyRequest(_))
+      .WillOnce(Return("test-request"));
+  auto data_channel = webrtc::MockDataChannelInterface::Create();
+  std::string sent_message;
+  EXPECT_CALL(*data_channel, SendAsync)
+      .WillOnce([&sent_message](
+                    webrtc::DataBuffer buffer,
+                    absl::AnyInvocable<void(webrtc::RTCError) &&> on_complete) {
+        sent_message = std::string(buffer.data.cdata<char>(), buffer.size());
+        std::move(on_complete)(webrtc::RTCError::OK());
+      });
+  ConferenceDataChannel conference_data_channel(std::move(resource_handler),
+                                                std::move(data_channel));
+
+  absl::Status status =
+      conference_data_channel.SendRequest(SessionControlChannelFromClient());
+
+  EXPECT_OK(status);
+  EXPECT_THAT(sent_message, StrEq("test-request"));
+}
+
+TEST(ConferenceDataChannelTest, SendRequestFailsWhenParsingFails) {
+  auto resource_handler = std::make_unique<MockResourceHandler>();
+  EXPECT_CALL(*resource_handler, StringifyRequest(_))
+      .WillOnce(Return(absl::InternalError("test-error")));
+  auto data_channel = webrtc::MockDataChannelInterface::Create();
+  EXPECT_CALL(*data_channel, SendAsync).Times(0);
+  ConferenceDataChannel conference_data_channel(std::move(resource_handler),
+                                                std::move(data_channel));
+
+  absl::Status status =
+      conference_data_channel.SendRequest(SessionControlChannelFromClient());
+
+  EXPECT_THAT(status, StatusIs(absl::StatusCode::kInternal, "test-error"));
+}
+
+TEST(ConferenceDataChannelTest, SendRequestLogsErrorWhenTransmissionFails) {
+  auto resource_handler = std::make_unique<MockResourceHandler>();
+  EXPECT_CALL(*resource_handler, StringifyRequest(_))
+      .WillOnce(Return("test-request"));
+  auto data_channel = webrtc::MockDataChannelInterface::Create();
+  EXPECT_CALL(*data_channel, SendAsync)
+      .WillOnce([](webrtc::DataBuffer /*buffer*/,
+                   absl::AnyInvocable<void(webrtc::RTCError) &&> on_complete) {
+        std::move(on_complete)(webrtc::RTCError(
+            webrtc::RTCErrorType::INTERNAL_ERROR, "test-error"));
+      });
+  ScopedMockLog log(kDoNotCaptureLogsYet);
+  std::string message;
+  EXPECT_CALL(log, Log(ERROR, _, _))
+      .WillOnce([&message](int, const std::string &, const std::string &msg) {
+        message = msg;
+      });
+  log.StartCapturingLogs();
+  ConferenceDataChannel conference_data_channel(std::move(resource_handler),
+                                                std::move(data_channel));
+
+  absl::Status status =
+      conference_data_channel.SendRequest(SessionControlChannelFromClient());
+
+  EXPECT_OK(status);
+  EXPECT_THAT(message, HasSubstr("test-error"));
+}
+
+TEST(ConferenceDataChannelTest,
+     CreatingConferenceDataChannelRegistersObserver) {
+  auto data_channel = webrtc::MockDataChannelInterface::Create();
+  webrtc::DataChannelObserver *observer;
+  EXPECT_CALL(*data_channel, RegisterObserver(_))
+      .WillOnce([&observer](webrtc::DataChannelObserver *inner_observer) {
+        observer = inner_observer;
+      });
+
+  ConferenceDataChannel conference_data_channel(
+      std::make_unique<MockResourceHandler>(), std::move(data_channel));
+
+  EXPECT_THAT(observer, NotNull());
+}
+
+TEST(ConferenceDataChannelTest, ReceivingUpdateSucceeds) {
+  auto resource_handler = std::make_unique<MockResourceHandler>();
+  EXPECT_CALL(*resource_handler, ParseUpdate(_))
+      .WillOnce(Return(SessionControlChannelToClient()));
+  auto data_channel = webrtc::MockDataChannelInterface::Create();
+  webrtc::DataChannelObserver *observer;
+  EXPECT_CALL(*data_channel, RegisterObserver(_))
+      .WillOnce([&observer](webrtc::DataChannelObserver *inner_observer) {
+        observer = inner_observer;
+      });
+  ConferenceDataChannel conference_data_channel(std::move(resource_handler),
+                                                std::move(data_channel));
+  ResourceUpdate received_update;
+  MockFunction<void(ResourceUpdate)> mock_function;
+  EXPECT_CALL(mock_function, Call)
+      .WillOnce([&received_update](ResourceUpdate update) {
+        received_update = std::move(update);
+      });
+  conference_data_channel.SetCallback(mock_function.AsStdFunction());
+
+  observer->OnMessage(webrtc::DataBuffer("test-update"));
+
+  EXPECT_TRUE(
+      std::holds_alternative<SessionControlChannelToClient>(received_update));
+}
+
+TEST(ConferenceDataChannelTest, ReceivingUpdateWithoutCallbackDoesNothing) {
+  auto data_channel = webrtc::MockDataChannelInterface::Create();
+  webrtc::DataChannelObserver *observer;
+  EXPECT_CALL(*data_channel, RegisterObserver(_))
+      .WillOnce([&observer](webrtc::DataChannelObserver *inner_observer) {
+        observer = inner_observer;
+      });
+  ConferenceDataChannel conference_data_channel(
+      std::make_unique<MockResourceHandler>(), std::move(data_channel));
+  ScopedMockLog log(kDoNotCaptureLogsYet);
+  std::string message;
+  EXPECT_CALL(log, Log(WARNING, _, _))
+      .WillOnce([&message](int, const std::string &, const std::string &msg) {
+        message = msg;
+      });
+  log.StartCapturingLogs();
+
+  observer->OnMessage(webrtc::DataBuffer("test-update"));
+
+  EXPECT_THAT(message,
+              HasSubstr("data channel received message but has no callback."));
+}
+
+TEST(ConferenceDataChannelTest, ReceivingUpdateFailsWhenReceivingBinaryData) {
+  auto data_channel = webrtc::MockDataChannelInterface::Create();
+  webrtc::DataChannelObserver *observer;
+  EXPECT_CALL(*data_channel, RegisterObserver(_))
+      .WillOnce([&observer](webrtc::DataChannelObserver *inner_observer) {
+        observer = inner_observer;
+      });
+  ConferenceDataChannel conference_data_channel(
+      std::make_unique<MockResourceHandler>(), std::move(data_channel));
+  conference_data_channel.SetCallback([](ResourceUpdate /*update*/) {});
+  ScopedMockLog log(kDoNotCaptureLogsYet);
+  std::string message;
+  EXPECT_CALL(log, Log(ERROR, _, _))
+      .WillOnce([&message](int, const std::string &, const std::string &msg) {
+        message = msg;
+      });
+  log.StartCapturingLogs();
+
+  observer->OnMessage(webrtc::DataBuffer("test-update", true));
+
+  EXPECT_THAT(message,
+              HasSubstr("data channel received unexpected binary update."));
+}
+
+TEST(ConferenceDataChannelTest, ReceivingUpdateFailsWhenParsingFails) {
+  auto resource_handler = std::make_unique<MockResourceHandler>();
+  EXPECT_CALL(*resource_handler, ParseUpdate(_))
+      .WillOnce(Return(absl::InternalError("parsing-error")));
+  auto data_channel = webrtc::MockDataChannelInterface::Create();
+  webrtc::DataChannelObserver *observer;
+  EXPECT_CALL(*data_channel, RegisterObserver(_))
+      .WillOnce([&observer](webrtc::DataChannelObserver *inner_observer) {
+        observer = inner_observer;
+      });
+  ConferenceDataChannel conference_data_channel(std::move(resource_handler),
+                                                std::move(data_channel));
+  conference_data_channel.SetCallback([](ResourceUpdate /*update*/) {});
+  ScopedMockLog log(kDoNotCaptureLogsYet);
+  std::string message;
+  EXPECT_CALL(log, Log(ERROR, _, _))
+      .WillOnce([&message](int, const std::string &, const std::string &msg) {
+        message = msg;
+      });
+  log.StartCapturingLogs();
+
+  observer->OnMessage(webrtc::DataBuffer("test-update"));
+
+  EXPECT_THAT(message, HasSubstr("parsing-error"));
+}
+
+}  // namespace
+}  // namespace meet
